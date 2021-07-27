a795cd71670982c5c87e0253e64731b29d2b9565
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-07-27 18:19:51 +0530

    App logic was implemented, after quite a struggle! Hope to write cleaner code for the same

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const container = document.querySelector('.container')
 
 
-function createGrid(){
+/*function createGrid(){
 for(let i=0;i<256;i++){
     const div_key = document.createElement('div');
-    div_key.setAttribute('class','square hover:bg-black')
+    div_key.setAttribute('class','square')
     container.appendChild(div_key)
 }}
-createGrid()
-
+createGrid()*/
+let colour = "black";
 const gridNo = document.querySelector('#boxes')
 gridNo.addEventListener('change',updateGrid)
 function updateGrid(){
     container.innerHTML = "";
+    if(gridNo.value < 0){
+      document.querySelector('#warning').setAttribute('class','block');
+    }
+    else
+    {
+    document.querySelector('#warning').setAttribute('class','hidden');
     let gridArea = gridNo.value**2
-    console.log(gridArea)
     container.style.setProperty(
         "grid-template-columns",
         `repeat(${gridNo.value}, 2fr)`
@@ -25,11 +30,38 @@ function updateGrid(){
       );
     for(let i=0;i<gridArea;i++){
         const div_ke = document.createElement('div');
-        div_ke.setAttribute('class','square hover:bg-black')
+        div_ke.setAttribute('class','square bg-gray-50 border border-gray-400')
         container.appendChild(div_ke)
-}}
-    
+}
+let square = document.querySelectorAll('.square');
+//let colour = "black";
+square.forEach((sq)=>{
+  sq.addEventListener('mouseover',(event)=>{
+    event.target.setAttribute('style',`background-color:${colour};`);
+  } 
+  );
+});
+}
+}
+const colorPicker = (color) => {
+  colour = color;
+  console.log(colour);
+}   
 const reset = document.querySelector('#reset')
 reset.addEventListener('click', function(){
     location.reload()
 })
+
+let square = document.querySelectorAll('.square');
+
+square.forEach((sq)=>{
+  sq.addEventListener('mouseover',(event)=>{
+    event.target.setAttribute('style',`background-color:${colour};`);
+  } 
+  );
+});
+
+/*const colorPicker = (color) => {
+    colour = color;
+    console.log(colour);
+}*/
